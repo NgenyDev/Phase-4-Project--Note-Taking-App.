7a58db353957071c0c5bfbe61c92868d23ca180d
Ngeny Gilbert <ngenygkip@gmail.com>
2024-10-23 17:21:08 +0300

    waawaa

--- models.py
+++ models.py
@@ -1,3 +1,104 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from sqlalchemy.exc import IntegrityError
+# from marshmallow import Schema, fields, validate
+
+# db = SQLAlchemy()
+# bcrypt = Bcrypt()
+
+
+# class User(db.Model):
+#     __tablename__ = 'users'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(150), unique=True, nullable=False)
+#     _password_hash = db.Column(db.String(128), nullable=False)
+
+#     @property
+#     def password(self):
+#         raise AttributeError('Password is not readable')
+
+#     @password.setter
+#     def password(self, password):
+#         self._password_hash = bcrypt.generate_password_hash(
+#             password).decode('utf-8')
+
+#     def check_password(self, password):
+#         return bcrypt.check_password_hash(self._password_hash, password)
+
+#     def __repr__(self):
+#         return f"<User email={self.email}>"
+
+
+# class Note(db.Model):
+#     __tablename__ = 'notes'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(255), nullable=False)
+#     content = db.Column(db.Text, nullable=False)
+#     tags = db.Column(db.String(255), nullable=True)  # Optional field for tags
+#     date_created = db.Column(db.DateTime, nullable=False,
+#                              default=db.func.current_timestamp())
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+#     user = db.relationship('User', backref=db.backref('notes', lazy=True))
+
+#     def __repr__(self):
+#         return f"<Note title={self.title}, user_id={self.user_id}>"
+
+
+# class EditorContent(db.Model):
+#     __tablename__ = 'editor_content'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     note_id = db.Column(db.Integer, db.ForeignKey('notes.id'), nullable=False)
+#     content = db.Column(db.Text, nullable=False)
+#     created_at = db.Column(db.DateTime, nullable=False,
+#                            default=db.func.current_timestamp())
+
+#     note = db.relationship('Note', backref=db.backref(
+#         'editor_contents', lazy=True))
+
+#     def __repr__(self):
+#         return f"<EditorContent for Note ID={self.note_id}>"
+
+
+# class ContactMessage(db.Model):
+#     __tablename__ = 'contact_us'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(150), nullable=False)
+#     email = db.Column(db.String(150), nullable=False)
+#     subject = db.Column(db.String(255), nullable=False)
+#     message = db.Column(db.Text, nullable=False)
+#     date_created = db.Column(db.DateTime, nullable=False,
+#                              default=db.func.current_timestamp())
+
+#     def __repr__(self):
+#         return f"<ContactMessage from={self.name}, subject={self.subject}>"
+
+# # Example Marshmallow Schemas for Validation
+
+
+# class UserSchema(Schema):
+#     id = fields.Int(dump_only=True)
+#     email = fields.Str(required=True, validate=validate.Email())
+#     password = fields.Str(required=True, load_only=True)
+
+
+# class NoteSchema(Schema):
+#     id = fields.Int(dump_only=True)
+#     title = fields.Str(required=True)
+#     content = fields.Str(required=True)
+#     tags = fields.Str(allow_none=True)
+
+
+# class ContactMessageSchema(Schema):
+#     id = fields.Int(dump_only=True)
+#     name = fields.Str(required=True)
+#     email = fields.Str(required=True, validate=validate.Email())
+#     subject = fields.Str(required=True)
+#     message = fields.Str(required=True)
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from sqlalchemy.exc import IntegrityError
